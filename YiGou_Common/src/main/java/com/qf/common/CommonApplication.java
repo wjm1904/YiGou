00cb8361e81ee986aafa1684fd1fd5d924bcf46d
wjm <1827444716@qq.com>
2019-09-20 22:54:28 +0800

    添加Common项目

---
+++ YiGou_Common/src/main/java/com/qf/common/CommonApplication.java
@@ -0,0 +1,20 @@
+package com.qf.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @PackgeName: com.qf.common
+ * @ClassName: CommonApplication
+ * @Author: Administrator
+ * Date: 2019/9/20 22:43
+ * project name: YiGou
+ * @Version:
+ * @Description:
+ */
+@SpringBootApplication
+public class CommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApplication.class, args);
+    }
+}
